27e20abddcc794a2a10178596307511eb168f0f8
Lázaro Ferreira <155861352+Lazaro-fe@users.noreply.github.com>
2026-04-30 02:20:28 +0000

    Salvando

--- Interface_IMC.py
+++ Interface_IMC.py
@@ -5,7 +5,7 @@ ctk.set_appearance_mode('white')
 # INICIO DE JANELA DE CALCÚLO DE IMC
 
 janela_IMC = ctk.CTk("Gray")
-janela_IMC.geometry('600X400')
+janela_IMC.geometry('600x400')
 janela_IMC.title('CTK')
 
 # CALCULO DE IMC
@@ -15,21 +15,39 @@ def calculo_de_IMC():
         valor_peso = float(peso.get())
         valor_altura = float(altura.get())
 
-        calculo_de_altura = altura * altura
-        calculo_final_de_imc = peso / calculo_de_altura
+        calculo_final_de_imc = valor_altura / (valor_altura * valor_altura)
 
         if(calculo_final_de_imc < 18.5):
-            classificação
+            classificacao = "Abaixo do Peso"
+        elif (18.5 <= calculo_final_de_imc < 24.9):
+            classificacao = "Peso Normal"
+        elif (25 <= calculo_final_de_imc < 29.9):
+            classificacao = "Sobrepeso"
+        else:
+            classificacao = "Obesidade"
+        
+        label_resultado.valor.configure(text= f"{calculo_final_de_imc:.2f}")
+        label_resultado_class.configure(text=classificacao)
+
+    except ValueError:
+        messagebox.showerror("Erro de Entrada!", "Por favor!\nTente inserir números válidos!")
 
 # ELEMENTOS DENTRO DA JANELA
 
+# TÍTULO
 ctk.CTkLabel(janela_IMC,
              text='Calculadora de IMC',
              text_color='Blue',
              font=('Arial', 45)).pack(pady=20)
 
+# SUBTÍTULO
+ctk.CTkLabel(janela_IMC,
+            text='Descubra seu índice de massa corporal',
+            text_color='gray',
+            font=('Arial', 32)).pack(pady=20)
+
 # BLOCO DE ESCRITA DO PESO
-peso = ctk.CTKEntry(janela_IMC,
+peso = ctk.CTkEntry(janela_IMC,
                     width=450,
                     height = 30,
                     placeholder_text= 'Digite seu peso em kg: ',
@@ -38,6 +56,7 @@ peso = ctk.CTKEntry(janela_IMC,
 
 peso.pack(pady=10)
 
+# BLOCO DE ESCRITA DE ALTURA
 altura = ctk.CTkEntry(janela_IMC,
                       width=450,
                       height=30,
@@ -45,3 +64,18 @@ altura = ctk.CTkEntry(janela_IMC,
                       border_width=2,
                       border_color='gray')
 
+altura.pack(pady=10)
+
+# BOTÃO DE CALCULO DE IMC
+
+botao_calcular = ctk.CTkButton(janela_IMC,
+                              text= 'Calcular IMC',
+                              fg_color= 'blue',
+                              text_color= 'white',
+                              height= 40,
+                              command= calculo_de_IMC)
+
+botao_calcular.pack(pady=10)
+
+# Irá Mudar o '--' para o valor do IMC
+label_resultado_valor = ctk.CTkLabel()
